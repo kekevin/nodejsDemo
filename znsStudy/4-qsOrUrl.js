014ff3f99a28ca7a6ba96d694aa25f833d8eb13e
kevin <linkkevin@qq.com>
2018-07-03 09:16:43 +0800

    url is for get

--- znsStudy/4-qsOrUrl.js
+++ znsStudy/4-qsOrUrl.js
@@ -5,6 +5,7 @@ http.createServer((req, res) => {
     //示例请求http://localhost:8880/s.html?ie=UTF-8&wd=aaaa
     //格式化请求可以用字符串操作splice("？")等获得数组，或者使用queryString,
     //最优先选择url，url.parse第二个参数，默认false格式化部分，true格式化所有请求参数
+    //这种解析是为了get请求的参数
     var obj = urlLib.parse(req.url, true)
     console.log('obj', obj)
     var url = obj.pathname
